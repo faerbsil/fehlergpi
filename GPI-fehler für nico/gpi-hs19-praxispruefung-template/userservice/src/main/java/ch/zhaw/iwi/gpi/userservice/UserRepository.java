86e9c6d0f37580d28099fca5b9656e762652c8d7
faerbsil <55694079+faerbsil@users.noreply.github.com>
2019-12-07 15:28:59 +0100

    Add files via upload

---
+++ GPI-fehler für nico/gpi-hs19-praxispruefung-template/userservice/src/main/java/ch/zhaw/iwi/gpi/userservice/UserRepository.java
@@ -0,0 +1,12 @@
+package ch.zhaw.iwi.gpi.userservice;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * UserRepository
+ */
+public interface UserRepository extends JpaRepository{
+    
+}
